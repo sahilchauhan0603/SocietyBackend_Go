8b7a7efba236013d6629851c29a06901c5fee97a
mohitgusain8671 <mohitgusain8671@gmail.com>
2024-07-21 20:21:18 +0530

    Fix Some errors

--- controllers/societyEventController.go
+++ controllers/societyEventController.go
@@ -53,6 +53,7 @@ func UpdateEvent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	database.DB.Save(&event)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(event)
 }
