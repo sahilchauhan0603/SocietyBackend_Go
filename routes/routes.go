9112ea01a26c8746c317b7b98c2a0ff872afc09d
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2024-07-23 23:35:16 +0530

    removed jwt

--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sahilchauhan0603/society/controllers"
-	"github.com/sahilchauhan0603/society/middleware"
+	// "github.com/sahilchauhan0603/society/middleware"
 )
 
 func InitializeRoutes(router *mux.Router) {
@@ -23,7 +23,7 @@ func InitializeRoutes(router *mux.Router) {
 
 	// User routes
 	r := router.PathPrefix("/api/v1").Subrouter()
-	r.Use(middleware.JWTVerify)
+	// r.Use(middleware.JWTVerify)
 	
 	r.HandleFunc("/user", controllers.CreateUser).Methods("POST")
 	r.HandleFunc("/user", controllers.GetUser).Methods("GET")
