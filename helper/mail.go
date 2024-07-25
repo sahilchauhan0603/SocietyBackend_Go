5c69ac2b079d6f35d018a82333974e3de189a3a6
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2024-07-25 19:49:13 +0530

    ContactUs form

--- helper/mail.go
+++ helper/mail.go
@@ -1,15 +1,34 @@
 package helper
 
-import "gopkg.in/gomail.v2"
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+)
 
 func SendEmail(recipient string, subject string, body string) error {
-	m := gomail.NewMessage()
 
-	m.SetHeader("From", "mohit84cseb22@bpitindia.edu.in")
+	smtpServer := os.Getenv("SMTP_SERVER")
+	smtpPort := os.Getenv("SMTP_PORT")
+	emailUser := os.Getenv("EMAIL_USER")
+	emailPassword := os.Getenv("EMAIL_PASSWORD")
+
+	// Convert SMTP_PORT to int
+	smtpPortInt, err := strconv.Atoi(smtpPort)
+	if err != nil {
+		log.Fatal("Invalid SMTP port")
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", emailUser)
 	m.SetHeader("To", recipient)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body)
-	d := gomail.NewDialer("smtp.office365.com", 587, "mohit84cseb22@bpitindia.edu.in", "Abhishek@1998")
+
+	// Set up SMTP server details
+	d := gomail.NewDialer(smtpServer, smtpPortInt, emailUser, emailPassword)
 
 	// Send the email
 	if err := d.DialAndSend(m); err != nil {
